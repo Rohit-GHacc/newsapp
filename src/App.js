04478af27d0a829e8c95481e977a2248b1fbccf5
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-03-12 13:41:47 +0530

    $ adding pageSize variable in app.js $ refactoring the newspanda app $ - adding default props and their required types by importing propTypes in news.js $ - passing props in constructor in news.js $ - adding updateNews function for nextClick/prevClick/componentDidMount in news.js $ - adding a function to capitalize the first letter of the category on headline of the news and title of the news in news.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import NavBar from './Components/NavBar';
 import News from './Components/News';
 import {
   BrowserRouter,
-  Link,
   Route,
   Routes,
 } from "react-router-dom";
 
 export default class App extends Component {
+  
+  pageSize = 15;
   render() {
     return (
       
@@ -22,14 +23,14 @@ export default class App extends Component {
         
       </div>
       <Routes>
-        <Route exact path="/"  element={<News key="general" pageSize='5' country= 'in' category ='general' />} />
-        <Route exact path="/general"  element={<News key="general" pageSize='5' country= 'in' category ='general' />} />
-        <Route exact path="/entertainment" element={<News key="entertainment" pageSize='5' country= 'in' category ='entertainment' />} />
-        <Route exact path="/sports"  element={<News key="sports" pageSize='5' country= 'in' category ='sports' />} />
-        <Route exact path="/business"  element={<News key="business" pageSize='5' country= 'in' category ='business' />} />
-        <Route exact path="/health"  element={<News key="health" pageSize='5' country= 'in' category ='health' />} />
-        <Route exact path="/science"  element={<News key="science" pageSize='5' country= 'in' category ='science' />} />
-        <Route exact path="/technology"  element={<News pageSize='5' key="technology" country= 'in' category ='technology' />} />
+        <Route exact path="/"  element={<News key="general" pageSize={this.pageSize} country= 'in' category ='general' />} />
+        <Route exact path="/general"  element={<News key="general" pageSize={this.pageSize} country= 'in' category ='general' />} />
+        <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country= 'in' category ='entertainment' />} />
+        <Route exact path="/sports"  element={<News key="sports" pageSize={this.pageSize} country= 'in' category ='sports' />} />
+        <Route exact path="/business"  element={<News key="business" pageSize={this.pageSize} country= 'in' category ='business' />} />
+        <Route exact path="/health"  element={<News key="health" pageSize={this.pageSize} country= 'in' category ='health' />} />
+        <Route exact path="/science"  element={<News key="science" pageSize={this.pageSize} country= 'in' category ='science' />} />
+        <Route exact path="/technology"  element={<News pageSize={this.pageSize} key="technology" country= 'in' category ='technology' />} />
         
       </Routes>
     </BrowserRouter>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,65 +1,100 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
+import PropTypes from 'prop-types'
 
 export class News extends Component {
-  articles= [];
-
-  constructor(){
-    super();
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+  }
+  static propTypes={
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+  constructor(props){
+    super(props);
     console.log('hello i am a constructor and i have been called');
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
       page: 1
     }
+    // changing the title of the page according to the categories of the news
+    document.title = `${this.capitalize(this.props.category)}-NewsPanda`;
+    
+    console.log('page size is :' + this.props.pageSize);
+ }
+ //function for capitalizing first letter below:
+ capitalize = (str)=>{
+  return str.charAt(0).toUpperCase() + str.slice(1);
  }
 
- async componentDidMount(){
-    console.log('cdm');
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=1&pageSize=${this.props.pageSize}`;
+ async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json()
     
-    console.log(parsedData);
-    this.setState({articles: parsedData.articles,loading: false, totalResults: parsedData.totalResults});
+    console.log('total results are :' + parsedData.totalResults);
+    this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false
+    })
+ }
+ async componentDidMount(){
+    // console.log('cdm');
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true})
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    
+    // console.log(parsedData);
+    // this.setState({articles: parsedData.articles,loading: false, totalResults: parsedData.totalResults});
+    this.updateNews();
 
  }
 handleNextClick = async ()=>{
     console.log('next button clicked')
 
-    if (!(this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize))) {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading: false
-        })
-    }
+    // if (!(this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize))) {
+    //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //     this.setState({loading: true})
+    //     let data = await fetch(url);
+    //     let parsedData = await data.json()
+    //     this.setState({
+    //         page: this.state.page + 1,
+    //         articles: parsedData.articles,
+    //         loading: false
+    //     })
+    // }
+    this.setState({page: this.state.page+1});
+    this.updateNews();
 
 }
  
 handlePrevClick = async()=>{
     console.log('previous button clicked')
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({
-        page: this.state.page-1,
-        articles: parsedData.articles,
-        loading: false
-    })
-    console.log('page = '+this.state.page);
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=efbaafb4252d4557b79529f0c3ed595a&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true})
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    // this.setState({
+    //     page: this.state.page-1,
+    //     articles: parsedData.articles,
+    //     loading: false
+    // })
+    // console.log('page = '+this.state.page);
+    this.setState({page: this.state.page -1 });
+    this.updateNews();
 }
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='text-center' style={{margin: '40px 25px', color: 'white'}}>NewsPanda - Top Headlines</h1>
+        <h1 className='text-center' style={{margin: '40px 25px', color: 'white'}}>NewsPanda - Top {this.capitalize(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
         {!this.state.loading && this.state.articles.map((element) => {
