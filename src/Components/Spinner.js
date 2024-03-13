dbb987343c2138689479cb537bb7325747da907d
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-03-13 15:57:33 +0530

    $ converted class based News.js to functional based News.js $ fixed the spinner on infinite scrolling

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,7 +3,7 @@ import loading from './spinner.gif'
 const Spinner = ()=> {
     return (
       <div className="text-center">
-        <img className="my-3" src={loading} alt="loading" />
+        <img className="my-3" src={loading} alt="loading" style={{height: '2rem'}}/>
       </div>
     ) 
 }
